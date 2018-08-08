2435356835b140ea8ee5e5c753c4dd60d66a9bac
AJ Bahnken <aj@ajvb.me>
2018-08-08 16:39:27 -0700

    Fixed the FIXME comment within the syslog parsing code

--- utils.py
+++ utils.py
@@ -61,16 +61,20 @@ class Parser(object):
         msg_len, syslog_msg_payload = payload.split(" ", maxsplit=1)
         msg_len = int(msg_len)
 
-        # TODO: This is a fun one, and this fix is very hacky and will not work if the log line has enough
-        #       unicode characters to push far enough into the next line that also has unicode characters.
-        # This fixes a problem where the message length counts all unicode characters a 2 characters.
-        # Would probably be a lot better to decode into ascii safely and properly
+        # FIXME
+        # There is a problem with directly using the MSGLEN in the current configuration. What seems to be
+        # happening is that when a syslog line is sent from Heroku, a unicode character is counted as having
+        # a length of 2. But, when the loglines are being processed within this python 3.6 code, all unicode
+        # characters have a length of 1.
+        #
+        # What exists below is a _bad hack_, but when working on this I was having trouble figuring out how to
+        # accomplish what I want to accomplish, which seems to be converting unicode into its control characters
+        # or similar for the purpose of parsing out the message.
         num_of_unicode_chars = len(
             [c for c in syslog_msg_payload[0:msg_len] if ord(c) > 128]
         )
         msg_len -= num_of_unicode_chars
 
-        # only grab msg_len bytes of syslog_msg
         syslog_msg = syslog_msg_payload[0:msg_len]
         next_payload = syslog_msg_payload[msg_len:]
 
