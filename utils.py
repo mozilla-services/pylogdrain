6a1fdf9e2ebcb362b71c2ece4a0d70227d3b7ec6
AJ Bahnken <aj@ajvb.me>
2018-08-09 10:21:51 -0700

    handle the fact that msglen is bytes len rather than str len

--- utils.py
+++ utils.py
@@ -61,27 +61,16 @@ class Parser(object):
         msg_len, syslog_msg_payload = payload.split(" ", maxsplit=1)
         msg_len = int(msg_len)
 
-        # FIXME
-        # There is a problem with directly using the MSGLEN in the current configuration. What seems to be
-        # happening is that when a syslog line is sent from Heroku, a unicode character is counted as having
-        # a length of 2. But, when the loglines are being processed within this python 3.6 code, all unicode
-        # characters have a length of 1.
-        #
-        # What exists below is a _bad hack_, but when working on this I was having trouble figuring out how to
-        # accomplish what I want to accomplish, which seems to be converting unicode into its control characters
-        # or similar for the purpose of parsing out the message.
-        num_of_unicode_chars = len(
-            [c for c in syslog_msg_payload[0:msg_len] if ord(c) > 128]
-        )
-        msg_len -= num_of_unicode_chars
+        # The msg_len is a bytes len, so we convert to bytes for the slicing step
+        syslog_msg_payload = syslog_msg_payload.encode('utf-8')
 
         syslog_msg = syslog_msg_payload[0:msg_len]
         next_payload = syslog_msg_payload[msg_len:]
 
-        yield syslog_msg
+        yield syslog_msg.decode('utf-8')
 
         if next_payload:
-            yield from self.get_chunk(next_payload)
+            yield from self.get_chunk(next_payload.decode('utf-8'))
 
     def parse_rfc6587(self, payload):
         """
